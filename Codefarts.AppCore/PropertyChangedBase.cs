00ec5df22481e9cca641189b51d35bc458ac5dce
Dean Lunz <deanlunz@createdbyx.com>
2016-11-13 23:40:56 -0800

    Initial Commit

---
+++ Codefarts.AppCore/PropertyChangedBase.cs
@@ -0,0 +1,103 @@
+﻿using System.Xml.Serialization;
+
+namespace Codefarts.AppCore
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// A base class that implements the infrastructure for property change notification and automatically performs UI thread marshalling.
+    /// </summary>
+    [DataContract]
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Creates an instance of <see cref = "PropertyChangedBase" />.
+        /// </summary>
+        public PropertyChangedBase()
+        {
+            this.IsNotifying = true;
+        }
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Enables/Disables property change notification.
+        /// Virtualized in order to help with document oriented view models.
+        /// </summary>
+        [XmlIgnore]
+        public virtual bool IsNotifying { get; set; }
+
+        /// <summary>
+        /// Raises a change notification indicating that all bindings should be refreshed.
+        /// </summary>
+        public virtual void Refresh()
+        {
+            this.NotifyOfPropertyChange(string.Empty);
+        }
+
+        /// <summary>
+        /// Notifies subscribers of the property change.
+        /// </summary>
+        /// <param name = "propertyName">Name of the property.</param>
+#if NET || SILVERLIGHT
+        public virtual void NotifyOfPropertyChange(string propertyName) {
+#else
+        public virtual void NotifyOfPropertyChange([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+#endif
+            if (this.IsNotifying && this.PropertyChanged != null)
+            {
+                PlatformProvider.Current.OnUIThread(() => this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName)));
+            }
+        }
+
+        /// <summary>
+        /// Notifies subscribers of the property change.
+        /// </summary>
+        /// <typeparam name = "TProperty">The type of the property.</typeparam>
+        /// <param name = "property">The property expression.</param>
+        public void NotifyOfPropertyChange<TProperty>(Expression<Func<TProperty>> property)
+        {
+            this.NotifyOfPropertyChange(this.GetMemberInfo(property).Name);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged" /> event directly.
+        /// </summary>
+        /// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private MemberInfo GetMemberInfo(Expression expression)
+        {
+            var lambda = (LambdaExpression)expression;
+
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+
+            return memberExpression.Member;
+        }                                   
+    }
+}
